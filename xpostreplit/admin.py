55504ec659de0f6d55dbb8b09bf0c7ea9f69cefa
Hikmet AKGUN <37314398-hikmetakgun94@users.noreply.replit.com>
2025-12-28 08:30:46 +0000

    created-deploy-settings-for-digital-ocean

--- xpostreplit/admin.py
+++ xpostreplit/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from xpostreplit.models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "file",
+    ]
