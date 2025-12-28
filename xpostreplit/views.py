058da2fcc01f9529249c309dafe28d4aca1d4a17
Hikmet AKGUN <37314398-hikmetakgun94@users.noreply.replit.com>
2025-12-28 10:54:24 +0000

    created-post-info-in-home-page

--- xpostreplit/views.py
+++ xpostreplit/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from xpostreplit.models import Post
 # Create your views here.
 
 
 def home_views(request):
-    context = dict()
+    posts = Post.objects.all()
+    if posts is not None:
+        posts = posts
+    else:
+        posts = None
+    context = dict(posts=posts, )
     return render(request, "main/xpost-home.html", context)
